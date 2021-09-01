fba53c9fe993ea8c1727ae5b5915783942806f4a
nduytung <nduytung.1611@gmail.com>
2021-09-01 22:23:26 +0700

    add expressjs

--- bai5/app.js
+++ bai5/app.js
@@ -8,6 +8,7 @@ const app = express(); //app chinh la 1 constance cua express
 app.listen(3000); //ko can toi localhost
 
 //app lang nghe su kien GET va san sang phan hoi
+//express se chay tu TREN XUONG DUOI, neu nhu no khong gap cac truong hop nao o tren, no se chay thang cuoi cung
 app.get("/", (req, res) => {
   res.send("<p> homepage </p>"); //su dung method nay thi ko can end, ko can set content type header
   //su dung .send, ta co the bo qua buoc set header va buoc end, ta cung cos the gui theo bat ky kieu du lieu nao ta muon
@@ -24,3 +25,12 @@ app.get("/about", (req, res) => {
 app.get("/about-us", (req, res) => {
   res.redirect("/about");
 });
+
+//sau khi express da chay tat ca nhung code tren nhung khong dc trigger, no se chay dong nay
+//dat app.use cuoi cung
+app.use(
+  /*USE this function for every request in the APP*/
+  (req, res) => {
+    res.sendFile("./views/notfound.html", { root: __dirname });
+  }
+);
